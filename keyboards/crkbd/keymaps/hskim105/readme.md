6ca48b910bed7a59c1fcb3d70937e312e211bc7a
Victor Kim <hskim105@protonmail.com>
2021-08-28 21:53:06 -0400

    hskim105: Initial commit - personal crkbd keymap

---
+++ keyboards/crkbd/keymaps/hskim105/readme.md
@@ -0,0 +1,7 @@
+# My custom keyboard
+
+## Tap dance
+https://thomasbaart.nl/2018/12/13/qmk-basics-tap-dance/
+
+## RGB Setup
+https://github.com/qmk/qmk_firmware/blob/master/keyboards/crkbd/readme.md
---
+++ keyboards/crkbd/keymaps/hskim105/rules.mk
@@ -0,0 +1,7 @@
+LTO_ENABLE          = yes
+MOUSEKEY_ENABLE     = yes
+OLED_DRIVER_ENABLE  = yes
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes
+TAP_DANCE_ENABLE    = yes
+VIA_ENABLE          = no    # Cannot be used with tap dance
